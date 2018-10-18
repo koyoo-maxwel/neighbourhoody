81b303f7aabe2a315724e3ec2fc9215e011dd908
koyoo--maxwel <koyoomaxwel@.com>
2018-10-18 20:57:32 +0300

    modified filds in models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -6,11 +6,9 @@ from django.contrib.auth.models import Group
 
 # Create your models here.
 
+
 class GroupExtend(models.Model):
     group = models.OneToOneField(Group)
-
-    def __str__(self):
-        return self.group
 
     def create_group(self):
         self.save
@@ -18,16 +16,16 @@ class GroupExtend(models.Model):
     def delete_group(self):
         self.delete
 
+    def __str__(self):
+        return self.group
 
 
+# The neighbour
 class Jirani(models.Model):
     name = models.CharField(max_length=90)
     location = models.CharField(max_length=90)
     count = models.IntegerField()
     admin = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.name
 
     def create_jirani(self):
         self.save
@@ -55,16 +53,16 @@ class Jirani(models.Model):
         count = cls.objects.all()
         return count
 
+    def __str__(self):
+        return self.name
 
 
+# jirani's busines
 class Business(models.Model):
     name = models.CharField(max_length=90)
     jirani = models.ForeignKey(Jirani, on_delete=models.CASCADE, null=True)
-    email = models.EmailField()
+    email = models.EmailField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.name
 
     def create_business(self):
         self.save
@@ -79,16 +77,19 @@ class Business(models.Model):
 
     @classmethod
     def searched_business(cls, search_term):
-        biz = cls.objects.filter(user__username__icontains=search_term)
-        return biz
+        business = cls.objects.filter(user__username__icontains=search_term)
+        return business
 
     @classmethod
     def update_business(cls):
         update = cls.objects.all()
         return update
 
+    def __str__(self):
+        return self.name
 
 
+# user profile model
 class Profile(models.Model):
     name = models.CharField(max_length=90)
     image = models.ImageField(upload_to='profile/', null=True, blank=True)
@@ -97,7 +98,6 @@ class Profile(models.Model):
     email = models.EmailField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
-    
     def create_profile(self):
         self.save
 
@@ -121,18 +121,14 @@ class Profile(models.Model):
         return str(self.image)
 
 
-
-
+# my jirani posts
 class Post(models.Model):
-    name = models.CharField(max_length=90, null=True)
+    title = models.CharField(max_length=90, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to='post/', null=True, blank=True)
     caption = models.CharField(max_length=100)
     jirani = models.ForeignKey(Jirani, on_delete=models.CASCADE, null=True)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
-
-    def __str__(self):
-        return self.title
 
     def create_post(self):
         self.save
@@ -149,3 +145,6 @@ class Post(models.Model):
     def get_profile_posts(cls, profile_id):
         profile_posts = Post.objects.filter(id=profile_id)
         return profile_posts
+
+    def __str__(self):
+        return self.title
