0933edd41dd7786071b566d082a54b51ab8ba2e5
koyoo--maxwel <koyoomaxwel@.com>
2018-10-19 07:49:22 +0300

    modified filds in models

--- neighbourhood/admin.py
+++ neighbourhood/admin.py
@@ -1,3 +1,10 @@
+
+
 from django.contrib import admin
+from . models import Jirani, Business, Profile, Post
 
 # Register your models here.
+admin.site.register(Jirani)
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Post)
